33d4da391cc0d9e8c08a7c28bbf538c82883bf60
Max Anderberg <maxanderberg@gmail.com>
2021-09-07 21:44:44 +0200

    removed old image and import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -9,15 +8,7 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" keywords={['gatsby', 'blog', 'react']}/>
     <h1>Hi</h1>
-    <p>Welcome to my blog.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+
     <p>
       <Link to="/blogposts">View all posts</Link> <br />
     </p>
