84e4b5e09bc917e2832dc051dd5c39a131484ae5
Max Anderberg <maxanderberg@gmail.com>
2021-09-15 18:10:35 +0200

    styled nav links

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,12 +12,12 @@ const IndexPage = ({ data }) => {
     <Layout>
       <SEO title="Home" />
       <section class="flex flex-row space-x-96 pt-10 pb-10 border-b-2 mb-10">
-        <section>
+        <section class="flex-grow">
           <h1 class="text-3xl text-gray-700 font-semibold">Max Anderberg</h1>
           <p>Welcome to my rudimentary blog. Mainly write about code stuff.</p>
         </section>
-        <StaticImage class="w-3/12" src="../images/profile_image.png" alt="The man himself!" />
-      </section>
+        <StaticImage class="w-3/12 flex-grow-0" src="../images/profile_image.png" alt="The man himself!" />
+      </section >
       <div class="grid gap-20 lg:grid-cols-2 md:grid md:grid-cols-1">
         {blogPosts.map(({ node }) => (
             <Post key={node.id} props={node}/>
