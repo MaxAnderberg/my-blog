7e818f6526d4ade3f609097a5abf8031e0a4cc50
Max Anderberg <maxanderberg@gmail.com>
2021-09-15 20:38:56 +0200

    reduced height of the card image and added hover shadow to read more button

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -6,14 +6,14 @@ const post = ({props}) => {
   console.log(props)
   return (
     <div class="max-w-sm px-1 rounded overflow-hidden">
-      <img class="w-full rounded-lg" src={props.image.fluid.src}/>
-      <section >
+      <img class="w-full h-56 rounded-lg" src={props.image.fluid.src}/>
+      <section class="mb-5">
         <h3 class="font-bold text-xl mb-2">{props.title}</h3>
         <p class="text-gray-700 text-base">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
         </p>
-        <p>
-          <Link class="inline-block bg-gray-200 rounded-lg px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"to={`/blogpost/${props.slug}`}>Read more 👈</Link>
+        <p class="mt-2">
+          <Link class="inline-block bg-gray-200 hover:shadow-lg rounded-lg px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"to={`/blogpost/${props.slug}`}>Read more 👈</Link>
         </p>
       </section>
     </div>
