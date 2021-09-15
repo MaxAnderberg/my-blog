e147d87d5454f76387c43b537122e1ea3bafcdb4
Max Anderberg <maxanderberg@gmail.com>
2021-09-15 17:57:40 +0200

    shifted the nav links to th left

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 
 const index = () => {
   return (
-    <nav class="flex flex-row justify-between text-left items-baseline py-4 px-6 bg-white shadow">
+    <nav class="flex flex-row text-left py-4 px-6 bg-white shadow">
         <Link class="text-2xl no-underline text-grey-darkest hover:text-blue-dark" to="/">Home</Link>
         <Link class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"  to="/about">About</Link>
         <Link class="text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"  to="/about">About</Link>
