f1787f3eff4330f0c3f1eabee6183f8d2f86969b
Max Anderberg <maxanderberg@gmail.com>
2021-09-22 19:32:57 +0200

    fixed how the classes were asigned to the elements

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from "gatsby";
+import './Nav.css';
 
 const index = () => {
-  const active = "blue";
+  
   return (
     <nav class="flex flex-row text-left pt-5 pb-1 px-6 bg-white shadow" >
       <section class="ml-5 mr-20 font-bold text-xl">
@@ -10,11 +11,12 @@ const index = () => {
       </section>
       <section>
         <Link
-        class="text-lg font-semibold mr-1 no-underline text-grey-darkest hover:underline "
+        className="text-lg font-semibold mr-1 no-underline text-grey-darkest hover:underline"
         to="/">
            Home
         </Link>
-        <Link class="text-lg font-semibold mr-1 no-underline text-grey-darkest hover:underline " to="/about">About</Link>
+        <Link className="text-lg font-semibold mr-1 no-underline text-grey-darkest hover:underline" 
+        to="/about">About</Link>
       </section>
     </nav>
   )
