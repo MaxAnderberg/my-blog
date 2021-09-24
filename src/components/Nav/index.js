8425abfa2413b143068ad2c71821c05c40e3ff03
Max Anderberg <maxanderberg@gmail.com>
2021-09-24 17:06:56 +0200

    made active link a slightly bigger

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 import './Nav.css';
 
 const index = () => {
-  const active = `font-extrabold`
+  const active = `font-extrabold text-base`
   return (
     <nav class="text-left pr-5 pt-5 pl-5 xl:mx-32">
       <section class="font-bold text-2xl mb-8 ">
@@ -11,12 +11,13 @@ const index = () => {
       </section>
       <section class='uppercase'>
         <Link
-        className="text-md font-semibold mr-1 no-underline text-grey-darkest hover:underline pr-2"
-        activeClassName={active}
-        to="/">
+          className="text-sm font-semibold mr-1 no-underline text-grey-darkest hover:underline pr-2"
+          activeClassName={active}
+          to="/"
+        >
           Home
         </Link>
-        <Link className="text-md font-semibold mr-1 no-underline text-grey-darkest hover:underline px-2"
+        <Link className="text-sm font-semibold mr-1 no-underline text-grey-darkest hover:underline px-2"
         activeClassName={active}
         to="/about">About</Link>
       </section>
