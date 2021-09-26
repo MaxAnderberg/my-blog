7e6b9e99e5b7b7058b0ca7c37b6079abdf484288
Max Anderberg <maxanderberg@gmail.com>
2021-09-26 22:25:37 +0200

    removed larger font on active link

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,7 +4,7 @@ import './Nav.css';
 import { motion } from 'framer-motion';
 
 const index = () => {
-  const active = `font-extrabold text-base`
+  const active = `font-extrabold`
   return (
     <nav class="text-left pr-5 pt-5 pl-5 xl:mx-32">
       <section class="font-bold text-2xl mb-8 ">
@@ -16,7 +16,7 @@ const index = () => {
           activeClassName={active}
           to="/"
         >
-          <motion.p class='inline-block' 
+          <motion.p class='inline-block tracking-wider	' 
           whileHover={{translateY:-2, color:'blue'}}
           transition={{ type:'spring', stiffness:500 }}
           >
@@ -26,7 +26,7 @@ const index = () => {
         <Link className="text-sm font-semibold mr-1 no-underline text-grey-darkest px-2 "
         activeClassName={active}
         to="/about">
-          <motion.p class='inline-block' 
+          <motion.p class='inline-block tracking-wider	' 
           whileHover={{translateY:-2, color:'blue'}}
           transition={{ type:'spring', stiffness:500}}
           default={{duration:2}}
