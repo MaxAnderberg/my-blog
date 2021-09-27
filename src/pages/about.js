03aafaa4a90813a6dd869f67a003e99c790edfb1
Max Anderberg <maxanderberg@gmail.com>
2021-09-27 16:26:18 +0200

    created an outline for the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,13 @@ const about = () => {
   return (
     <Layout>
       <Seo title="About" />
-      <h1>About page</h1>
+      <h1>Hi there!</h1>
+      <p>
+        Some text to go there
+      </p>
+      <h1></h1>
+      <h1>About</h1>
+      <h1>About</h1>
   </Layout>
   )
 }
