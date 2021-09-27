b8500cf75c69c078157b38f5734b62310a83a7e5
Max Anderberg <maxanderberg@gmail.com>
2021-09-27 09:33:30 +0200

    turned component into links

---
+++ src/components/Nav/NavLink/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from "gatsby";
+import { motion } from 'framer-motion';
+
+const index = ({ text, destination }) => {
+  const active = `font-extrabold`
+  return (
+    <Link className="text-sm font-semibold mr-1 no-underline text-grey-darkest px-2 "
+    activeClassName={active}
+    to={destination}>
+      <motion.p class='inline-block tracking-wider	' 
+      whileHover={{translateY:-2, color:'blue'}}
+      transition={{ type:'spring', stiffness:500}}
+      default={{duration:2}}
+      >
+        {text}
+      </motion.p>
+    </Link>
+  )
+}
+
+export default index
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from "gatsby";
 import './Nav.css';
 import { motion } from 'framer-motion';
+import NavLink from './NavLink';
 
 const index = () => {
   const active = `font-extrabold`
@@ -11,29 +12,8 @@ const index = () => {
         <Link to='/'>&lt;max/&gt;</Link>
       </section>
       <section class='uppercase'>
-        <Link
-          className="text-sm font-semibold mr-1 no-underline text-grey-darkest pr-2"
-          activeClassName={active}
-          to="/"
-        >
-          <motion.p class='inline-block tracking-wider	' 
-          whileHover={{translateY:-2, color:'blue'}}
-          transition={{ type:'spring', stiffness:500 }}
-          >
-            Home
-          </motion.p>
-        </Link>
-        <Link className="text-sm font-semibold mr-1 no-underline text-grey-darkest px-2 "
-        activeClassName={active}
-        to="/about">
-          <motion.p class='inline-block tracking-wider	' 
-          whileHover={{translateY:-2, color:'blue'}}
-          transition={{ type:'spring', stiffness:500}}
-          default={{duration:2}}
-          >
-            About
-          </motion.p>
-        </Link>
+        <NavLink text='Home' destination='/' />
+        <NavLink text='About' destination='/about' />
       </section>
     </nav>
   )
