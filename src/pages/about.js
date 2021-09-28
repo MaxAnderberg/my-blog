e2a70274c9eca4c2ff88abbb200f9ef050e00a90
Max Anderberg <maxanderberg@gmail.com>
2021-09-28 11:50:11 +0200

    gave about page some structure

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,25 +15,36 @@ const about = () => {
   return (
     <Layout>
       <Seo title="About" />
-      <h1>Hey There (Wavy Hand)</h1>
-      <p>
-        My name is Max Anderberg and I'm a fullstack JavaScript developer. 
-        <br/>
-        <br/>
-        This blog is primarly for myself, like a diary. It gives me an oppertunity to maintain sideprojects which allows me to pursue and research new technologies,
-        refining old concepts etc. 
-      </p>
-      <p>Interested in some of my other works? Check out my portfolio</p>
-      <h1>Blog's Tech Stack</h1>
-      <p>
-        <Contentful width='50'/>
-        <Tailwindcss width='50'/>
-        <Gatsby width='50'/>
-        <Styledcomponents width='50'/>
-        <Javascript width='50'/>
-        <ReactLogo width='50'/>
-        <Nodejs width='50'/>
-      </p>
+      <main>
+        <h1>Hey There (Wavy Hand)</h1>
+        <p>
+          My name is Max Anderberg and I'm a fullstack JavaScript developer. 
+          <br/>
+          <br/>
+          This blog is primarly for myself, like a diary. It gives me an oppertunity to maintain sideprojects which allows me to pursue and research new technologies,
+          refining old concepts etc. 
+        </p>
+
+        <section>
+          <h1>Blog's Tech Stack</h1>
+
+          <section class='flex justify-evenly items-start'>
+            <Contentful width='50'/>
+            <Tailwindcss width='50'/>
+            <Gatsby width='50'/>
+            <Styledcomponents width='50'/>
+            <Javascript width='50'/>
+            <ReactLogo width='50'/>
+            <Nodejs width='50'/>
+          </section>
+          <p>Interested in looking at the source code? Check it out here on GitHub</p>
+        </section>
+        
+        <section>
+        <h1>Other Work</h1>
+        <p>Interested in some of my other works? Check out my portfolio</p>
+        </section>
+      </main>
 
   </Layout>
   )
