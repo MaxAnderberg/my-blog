c6fe7b8dccd913251b03c63713b8ec7846b089e4
Max Anderberg <maxanderberg@gmail.com>
2021-09-28 11:15:35 +0200

    navlinks properly aligned

--- src/components/Nav/NavLink/index.js
+++ src/components/Nav/NavLink/index.js
@@ -5,10 +5,10 @@ import { motion } from 'framer-motion';
 const index = ({ text, destination }) => {
   const active = `font-extrabold`
   return (
-    <Link className="text-sm font-semibold mr-1 no-underline text-grey-darkest px-2  "
+    <Link className="text-sm font-semibold mr-1 no-underline text-grey-darkest px-2 "
     activeClassName={active}
     to={destination}>
-      <motion.p class='inline-block tracking-wider	' 
+      <motion.p class='inline-block tracking-wider mb-0' 
       whileHover={{translateY:-2, color:'blue'}}
       transition={{ type:'spring', stiffness:500}}
       default={{duration:2}}
