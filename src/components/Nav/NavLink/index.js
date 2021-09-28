78df931959ebbe492c115a3203a521f201659dea
Max Anderberg <maxanderberg@gmail.com>
2021-09-28 15:44:29 +0200

    removed unused variable

--- src/components/Nav/NavLink/index.js
+++ src/components/Nav/NavLink/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 import { motion } from 'framer-motion';
 
 const index = ({ text, destination }) => {
-  const active = `font-extrabold text-warmdark`
+
   return (
     <Link className="text-sm font-semibold mr-1 no-underline px-2 text-lightgray "
     activeStyle={{color: '#231b15', fontWeight: 800}}
