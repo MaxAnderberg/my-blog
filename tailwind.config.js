2d7a60194d74ec6fee3ea2be8c2c69c38937e592
Max Anderberg <maxanderberg@gmail.com>
2021-09-28 13:22:57 +0200

    added more custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,17 +10,30 @@ module.exports = {
     extend: {
       fontFamily: {
         nunito: ['Nunito', 'Sans'],
-        roboto: ['Roboto', 'Sans'],
+        roboto: ['Roboto', 'Sans'], 
         monoton: ['Monoton', 'Sans'],
         mono: ['Roboto Mono', 'Sans'],
       }
     },
     colors: {
-      cg: {
-        light: '#f5f2f0',
+      cgw: {
+        light: '#f1f6f8',
         medium: '#f1ece9',
         dark: '#d5d0cd',
-      }
+      },
+      cgwt: {
+        light: '#564b43',
+        dark: '#231b15',
+      },
+      cgc: {
+        light: '#f5f2f0',
+        medium: '#ecf1f4',
+        dark: '#d9e3e8',
+      },
+      cgct: {
+        light: '#222b2f',
+        dark: '#19272f',
+      },
     }
   },
   variants: {
