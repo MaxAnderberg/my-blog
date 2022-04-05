62fd956e08f2b5154a4921688eeedfcc009b836a
Max Anderberg <maxanderberg@gmail.com>
2022-04-05 21:06:08 +0200

    added dependencies

--- src/components/Animated/SpinningSun/SpinningSun.js
+++ src/components/Animated/SpinningSun/SpinningSun.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-const SpinningHand = () => {
-  return (
-    <motion.div
-      style={{
-        marginBottom: '-20px',
-        marginRight: '-45px',
-        paddingBottom: '20px',
-        paddingRight: '45px',
-        display: 'inline-block',
-      }}
+const SpinningHand = () => (
+  <motion.div
+    style={{
+      marginBottom: '-20px',
+      marginRight: '-45px',
+      paddingBottom: '20px',
+      paddingRight: '45px',
+      display: 'inline-block',
+    }}
       // initial={{ opacity: 0 }}
       // exit={{ opacity: 1 }}
-      animate={{
-        rotate: 360,
-        opacity: [0.5, 1, 0, 0, 0.5],
-      }}
-      transition={{
-        repeat: Infinity,
-        ease: 'easeInOut',
-        type: 'tween',
-        duration: '6',
-      }}
-    >
-      ☀️
-    </motion.div>
-  );
-};
+    animate={{
+      rotate: 360,
+      opacity: [0.5, 1, 0, 0, 0.5],
+    }}
+    transition={{
+      repeat: Infinity,
+      ease: 'easeInOut',
+      type: 'tween',
+      duration: '6',
+    }}
+  >
+    ☀️
+  </motion.div>
+);
 
 export default SpinningHand;
