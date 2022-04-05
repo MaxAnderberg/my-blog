f2d8c78c7cdd59198bef879beb2f5e2da045ce0a
Max Anderberg <maxanderberg@gmail.com>
2022-04-05 20:57:56 +0200

    linted NavLink

--- src/components/Nav/NavLink/index.js
+++ src/components/Nav/NavLink/index.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import { Link } from "gatsby";
+import React from 'react';
+import { Link } from 'gatsby';
 import { motion } from 'framer-motion';
 
-const index = ({ text, destination }) => {
+const index = ({ text, destination }) => (
+  <Link
+    className="text-sm font-semibold mr-1 no-underline px-2 text-lightgray "
+    activeStyle={{ color: '#231b15', fontWeight: 800 }}
+    to={destination}
+  >
+    <motion.p
+      class="inline-block tracking-wider mb-0"
+      whileHover={{ translateY: -2, color: '#231b15' }}
+      transition={{ type: 'spring', stiffness: 500 }}
+    >
+      {text}
+    </motion.p>
+  </Link>
+);
 
-  return (
-    <Link className="text-sm font-semibold mr-1 no-underline px-2 text-lightgray "
-    activeStyle={{color: '#231b15', fontWeight: 800}}
-    to={destination}>
-      <motion.p class='inline-block tracking-wider mb-0' 
-      whileHover={{translateY:-2, color:'#231b15'}}
-      transition={{ type:'spring', stiffness:500}}
-      >
-        {text}
-      </motion.p>
-    </Link>
-  )
-}
-
-export default index
+export default index;
